3ff2562b02203adc5f92751017a4a21af9f3158e
enough <enough@80390d4c-0750-11df-8d27-21ff5afbef34>
2004-02-23 15:50:36 +0000

    Very basic state but a simple sample midlet is now being created

---
+++ trunk/enough-polish-dict/source/src/de/enough/polish/dict/UnusedClass.java
@@ -0,0 +1,147 @@
+/*
+ * Created on 23-Feb-2004 at 14:12:59.
+ * This source code is published under the GNU General Public Licence and
+ * the enough-software-licence for commercial use.
+ * Please refer to accompanying LICENSE.txt or visit www.enough.de for details.
+ */
+package de.enough.polish.dict;
+
+/**
+ * <p>This class has no purpose - it should only be removed but he obfuscator.</p>
+ *
+ * <p>copyright enough software 2004</p>
+ * <pre>
+ * history
+ *        23-Feb-2004 - rob creation
+ * </pre>
+ * @author Robert Virkus, robert@enough.de
+ */
+public class UnusedClass {
+	
+	String attribute1 = "asfsdjhfkdjshfjkdshfjkdshjkfdshjkdfshfjksdhfla45645euwsdfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjsdklfj25fh sdklfjsdklfjsdkljfklsdjfisdoujlksjdfkljsdkljfsdkljklsdjfkljsdiovnjcjkln";
+	String attribute2 = "asfsdjhfkdjshfjkdshfjkdshjkfdshjkdfshfjksdhflaeuwsdfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjssdfddklfjsdklfjsdklfjsdkljfklsdssdfjfisdoujlksjdfkljsdkljfsdkljklsdjfkljsdiovnjcjkln";
+	String attribute3 = "asfsdjhfkdjshfjkdshfjkdshjkfdshjkdfshfjksdhflaeuws45645dfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjsdklfjsdklfjsdklfjsdkljsdfdklsddjfisdoujlksjdfkljsnmbbfncvbsdkljfsdkljklsdjfkljsdiovnjcjkln";
+	String attribute4 = "asfsdjhfkdjs56756hfjkdshfjkdshjkfdshjkdfshfjksdhflaeuwsdfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjsdklfjsdklfjsdklfjsdkljfklsdjfisdoujlksjdfkljsdkljfsdkljklsdjfkljsdiovnjcjkln";
+	String attribute5 = "asfsdjhfkdjshfjkdshfjkdshjkfd78678hjkdfshfjksdhflaeuwsdfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjsdklfjsdklfjsdklfjsdkljfklsdjfisdoujlksjdfkljsdkljfsdkljklsdjfkljsdiovnjcjkln";
+	String attribute6 = "asfsdj234234hfkdjshfjkdshfjkdshjkfdshjkdfshfjksdhflaeu45645645wsdfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjsdklfjssdfsdfdklfjsdklfjsdkljfklsdjfisdoujlksjdfkljsdkljfsdkljklsdjfkljsdiovnjcjkln";
+	String attribute8 = "asfsdjhfkdjshfjkds234324hfjkdshjkfdshjkdfshfjksdhflaeuwsdfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjsdklfjsdklfjsdklfjsdkljfklsdjfisdoujlksjdfkljsdkhbfjhjljfsdkljklsdjfkljsdiovnjcjkln";
+	String attribute9 = "asfsdjhfkdjshfjkdshfjkdshjkfdshjkdfshfjk4564565sdhflaeuwsdfm,cn sdfiljfsdkljfdskl fdslkfjwioucjklxnm cxopdjkweopjclskjfusdfjkljsdfjsdxcvxcvklfjsdklfjsdklfjsdkljfklsdjfisdoujlksjdfkljsdkljfsdkljklsdjfkljsdiovnjcjkln";
+	
+	static final int[] VALUES = new int[]{ 1, 2,3,4,5,56,6,7,87,8,89,9,2,23,2,35,456,567,67, 4}; 
+	static final long[] VALUES2 = new long[]{ 1, 2,3,4,5,56,6,7,87,8,89,9,2,23,2,35,456,567,67, 4}; 
+	/**
+	 * 
+	 */
+	public UnusedClass() {
+		super();
+		// TODO enough implement UnusedClass
+	}
+	/**
+	 * @return Returns the vALUES.
+	 */
+	public static int[] getVALUES() {
+		return VALUES;
+	}
+	/**
+	 * @return Returns the vALUES2.
+	 */
+	public static long[] getVALUES2() {
+		return VALUES2;
+	}
+	/**
+	 * @return Returns the attribute1.
+	 */
+	public String getAttribute1() {
+		return this.attribute1;
+	}
+/**
+ * @param attribute1 The attribute1 to set.
+ */
+public void setAttribute1(String attribute1) {
+this.attribute1 = attribute1;}
+
+	/**
+	 * @return Returns the attribute2.
+	 */
+	public String getAttribute2() {
+		return this.attribute2;
+	}
+/**
+ * @param attribute2 The attribute2 to set.
+ */
+public void setAttribute2(String attribute2) {
+this.attribute2 = attribute2;}
+
+	/**
+	 * @return Returns the attribute3.
+	 */
+	public String getAttribute3() {
+		return this.attribute3;
+	}
+/**
+ * @param attribute3 The attribute3 to set.
+ */
+public void setAttribute3(String attribute3) {
+this.attribute3 = attribute3;}
+
+	/**
+	 * @return Returns the attribute4.
+	 */
+	public String getAttribute4() {
+		return this.attribute4;
+	}
+/**
+ * @param attribute4 The attribute4 to set.
+ */
+public void setAttribute4(String attribute4) {
+this.attribute4 = attribute4;}
+
+	/**
+	 * @return Returns the attribute5.
+	 */
+	public String getAttribute5() {
+		return this.attribute5;
+	}
+/**
+ * @param attribute5 The attribute5 to set.
+ */
+public void setAttribute5(String attribute5) {
+this.attribute5 = attribute5;}
+
+	/**
+	 * @return Returns the attribute6.
+	 */
+	public String getAttribute6() {
+		return this.attribute6;
+	}
+/**
+ * @param attribute6 The attribute6 to set.
+ */
+public void setAttribute6(String attribute6) {
+this.attribute6 = attribute6;}
+
+	/**
+	 * @return Returns the attribute8.
+	 */
+	public String getAttribute8() {
+		return this.attribute8;
+	}
+/**
+ * @param attribute8 The attribute8 to set.
+ */
+public void setAttribute8(String attribute8) {
+this.attribute8 = attribute8;}
+
+	/**
+	 * @return Returns the attribute9.
+	 */
+	public String getAttribute9() {
+		return this.attribute9;
+	}
+/**
+ * @param attribute9 The attribute9 to set.
+ */
+public void setAttribute9(String attribute9) {
+this.attribute9 = attribute9;}
+
+}
