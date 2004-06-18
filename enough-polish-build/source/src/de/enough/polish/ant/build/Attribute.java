1613f129e7c2e1c1e663810c0ac573fa74f57c10
enough <enough@80390d4c-0750-11df-8d27-21ff5afbef34>
2004-06-18 14:22:53 +0000

    Small fixes

--- enough-polish-build/source/src/de/enough/polish/ant/build/Attribute.java
+++ enough-polish-build/source/src/de/enough/polish/ant/build/Attribute.java
@@ -1,18 +1,34 @@
 /*
  * Created on Jun 18, 2004
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * This file is part of J2ME Polish.
+ *
+ * J2ME Polish is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * J2ME Polish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Commercial licenses are also available, please
+ * refer to the accompanying LICENSE.txt or visit
+ * http://www.j2mepolish.org for details.
  */
 package de.enough.polish.ant.build;
 
 import de.enough.polish.Variable;
 
 /**
- * @author robertvirkus
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * An attribute is a variable, which can also contain a condition.
+ * 
+ * @author robert virkus, j2mepolish@enough.de
  */
 public class Attribute extends Variable {
 
